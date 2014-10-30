ed59ad38cc77297a08a1546e9715a9f9b6760d55
Satinder Singh <satindersingh71@gmail.com>
2014-10-30 00:00:42 -0700

    Utopian Tree
    https://www.hackerrank.com/challenges/utopian-tree

---
+++ tree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int height(int n) {
+    int result = 1; // 3 --> 6
+    bool flag = false;
+    for(int i=0; i < n; i++)
+    {
+        if (flag) result++;
+        else result = result << 1; // much quicker using <<
+        flag = !flag;
+    }
+    
+    return result;
+}
+int main() {
+    int T,n;
+    cin >> T;
+    while (T--) {
+        cin >> n;
+        cout << height(n) << endl;
+    }
+}
